87e83353b61987c4a0735db3319e80860567c356
Zachary McCaw <zmccaw@Zacharys-MacBook-Pro.local>
2018-09-11 14:49:22 -0400

    First commit

---
+++ R/PackageHelp.R
@@ -0,0 +1,12 @@
+#' @useDynLib MatrixOps
+#' @importFrom Rcpp sourceCpp
+NULL
+
+#' MatrixOps: Matrix Operations
+#' 
+#' Package to store matrix operations implemented in Rcpp. 
+#' 
+#' @author Zachary R. McCaw
+#' @docType package
+#' @name MatrixOps
+NULL
